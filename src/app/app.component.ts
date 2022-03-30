1fef47cd753ae60927daa1438fd90f21532a5a72
Romeu <romeu.esp@gmail.com>
2022-03-30 20:45:20 +0200

    :wrench: gitignore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,18 +4,18 @@ import { Todo } from 'src/models/todo.model';
 
 @Component({
   // O decorator @Component utiliza o 'selector' pra gerar uma tag HTML na qual se usará para injetar todo o conteúdo do nosso component, tanto lógica como estilização. 
-  selector: 'app-root', 
+  selector: 'app-root',
   // Usa o templateUrl para externalizar o HTML 
-  templateUrl: './app.component.html', 
+  templateUrl: './app.component.html',
   // array que unifica todos os estilos CSS 
-  styleUrls: ['./app.component.css'] 
+  styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
   // empty array; o tipo any permite inserir qualquer tipo de dados dentro do array.
-  public todos: Todo[] = []; 
-  public title: string = 'Minhas Tarefas'; 
-  public form: FormGroup ;   
+  public todos: Todo[] = [];
+  public title: string = 'Minhas Tarefas';
+  public form: FormGroup;
 
   /**
    * method chamado ao inicializar o component
@@ -27,31 +27,69 @@ export class AppComponent {
         Validators.maxLength(60),
         Validators.required
       ])]
-    }); 
+    });
+
+    this.loadData();
   }
- 
- 
-  add() { 
-   const title = this.form.controls['title'].value; 
-   const id = this.todos.length + 1; 
-   this.todos.push(new Todo(id , title , false)); 
-   this.clear(); 
- }
- clear() { 
-   this.form.reset(); 
- }
-  remove(todo: Todo) { 
-    const index = this.todos.indexOf(todo); 
-    if (index !== -1)  { 
-      this.todos.splice(index, 1); 
+
+  /**
+   * 
+   */
+  add() {
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, title, false));
+    this.saveData();
+    this.clear();
+  }
+  /**
+   * metodo para limpar o input uma evz adicionada uma tarefa
+   */
+  clear() {
+    this.form.reset();
+  }
+  /**
+   * 
+   * @param todo => uma vez chamada a ação direta do HTML elimina o todo 
+   * selecionado do array de Todos.
+   */
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    if (index !== -1) {
+      this.todos.splice(index, 1);
     }
   }
 
-  markAsDone(todo: Todo) { 
-    todo.done= true ; 
+  /**
+   * @param todo => pega a lista de tarefas e uma vez é chamada a ação do 
+   * HTML muda o estado do Todo para True
+   */
+  markAsDone(todo: Todo) {
+    todo.done = true;
   }
 
-  markAsUndone(todo: Todo){
-    todo.done=false;
+  /**
+   * @param todo => pega o lista de Todo e uma vez é chamada a ação do 
+   * HTML muda o estado do Todo para false 
+   */
+  markAsUndone(todo: Todo) {
+    todo.done = false;
+  }
+
+  /**
+   * add data at localStorage in JsonStringfy format
+   */
+  saveData() {
+    const setData = JSON.stringify(this.todos);
+    localStorage.setItem('todos', setData);
+  }
+
+  /**
+   * pega o array de todos armazenados no LocalStorage e carrega quando
+   * a página carrega
+   */
+  loadData() {
+    const getData = localStorage.getItem('todos');
+    this.todos = JSON.parse(getData);
   }
 }
